33d2f224ee2ef9d22850e8cd8a3f4b7dba809527
Kenneth M <martinckenneth@gmail.com>
2020-01-27 15:32:35 -0800

    change

--- Lab_01/Lab _1_tests.R
+++
@@ -1,9 +0,0 @@
-montystay = function(i)
-{
-  # set up vector of doors
-  doors = c(1,2,3)
- # randomly pick where the car is
- cardoor = sample(doors, 1, replace = FALSE, prob = NULL)
-}
-
-results = sapply(montystay)
